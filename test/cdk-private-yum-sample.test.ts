32c382fd62229e241f6bc72c33d891c4771c48dd
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2023-05-25 11:26:26 +0900

    added snapshot test

--- test/cdk-private-yum-sample.test.ts
+++ test/cdk-private-yum-sample.test.ts
@@ -1,17 +1,13 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkPrivateYumSample from '../lib/cdk-private-yum-sample-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { CdkPrivateYumSampleStack } from '../lib/cdk-private-yum-sample-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-private-yum-sample-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkPrivateYumSample.CdkPrivateYumSampleStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe("CdkPrivateYumSample", () => {
+  test("matches the snapshot", () => {
+    const app = new cdk.App();
+    const stack = new CdkPrivateYumSampleStack(app, 'CdkPrivateYumSampleStack');
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    const template = Template.fromStack(stack);
+    expect(template.toJSON()).toMatchSnapshot();
+  });
 });
